cc21f5236ff5f3b13368dfe179772824afdc2d1e
quasi-quirky <maysacodesthecloud@gmail.com>
2025-06-16 19:43:29 -0400

    Add Hello_World.py script for user interaction with personalized greeting

---
+++ Hello_World.py
@@ -0,0 +1,8 @@
+# First, print a general welcome message to the user
+print("Welcome to this interactive program!")
+
+# Next, ask the user for their name and store their input in a variable
+user_name = input("Please enter your name: ")
+
+# Finally, use the entered name to give a personalized greeting
+print(f"Hello, {user_name}! It's great to meet you.")
