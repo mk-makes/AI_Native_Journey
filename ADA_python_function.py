111162471d924eebffa923cc2bf42b994836781a
quasi-quirky <maysacodesthecloud@gmail.com>
2025-06-28 04:32:33 -0400

    Revert greeting in Hello_World.py back to MK and refine scoring system and user feedback in Python_Variable_Matching_Game.py.

---
+++ ADA_python_function.py
@@ -0,0 +1,5 @@
+def get_user_words():
+    noun = input("Please enter a noun: ")
+    verb = input("Please enter a verb: ")
+    adjective = input("Please enter an adjective: ")
+    return noun, verb, adjective
